4a5d78ba002a3a8a1ede0f2cee83c0a3fff8dc00
AlexanderVikenfalk <alexander.wikenfalk@gmail.com>
2017-01-13 21:12:45 +0100

    Started with the responsivness in the design. Added Date, Runtime, and genres. Discovered a major bug: the results showed the input and not the recommendation - this was resolved.

--- Backup/v1/js/script.js
+++ Backup/v1/js/script.js
@@ -2,33 +2,40 @@
 
 $main = $("body");
 
-$(document).on({
-    ajaxStart: function (ajaxSearch) { $main.addClass("loading2"); },
-    ajaxStop: function (ajaxStop) { $main.removeClass("loading2"); }
-});
+// $(document).on({
+//     ajaxStart: function(ajaxSearch) { $main.addClass("loadingSymbol"); },
+//     ajaxStop: function(ajaxStop) { $main.removeClass("loadingSymbol"); }
+// });
 
 
 
-$(document).ready(function () {
+$(document).ready(function() {
 
     // Adjusts the window size when document is loaded (needed for background to work properly in Edge and Firefox)
-    $(function () {
+    $(function() {
         $(".fill-screen").css("height", window.innerHeight);
     });
 
     //  Adjusts the background image after windows size when window size is changed
-    $(window).on("load resize", function () {
+    $(window).on("load resize", function() {
         $(".fill-screen").css("height", window.innerHeight);
     });
 
-    // Method to clear all earlier results
-    var clearAll = function () {
+    // Method to clear all earlier results from .clear-classes
+    var clearAll = function() {
         var full = $(".clean");
         $('.clean').empty();
     }
 
+    // var clearPoster = function() {
+    //     var full = $(".cleanPoster");
+    //     $('.cleanPoster').empty();
+    //     $('.poster').html('<img id="thePoster" class="img-responsive clean" src="img/placeholder.png" />');
+
+    // }
+
     //Main method for getting poster
-    var getPoster = function () {
+    var getPoster = function() {
         var imagesize = "w342/"
         var poster_base_url = "https://image.tmdb.org/t/p/";
         var film = $('#term').val();
@@ -38,20 +45,20 @@ $(document).ready(function () {
         var recommendation;
         var recommendationMovieId;
         var director;
+        var recommendationPosterPath;
 
         // $( "#info" ).empty();
         clearAll();
 
         if (film == '') {
-            $('#poster').html("<h2 class='loading'>Please type something :-) </h2>");
+            $('#recommendation').html("<h2 class='loading'>Please type something :-) </h2>");
+            $("#thePoster").attr("src", "img/placeholder.png");
         } else {
-            $("#loadingDiv").show();
-
             $('#recommendation').html("<h2 class='loading'>We're Looking for your poster!</h2>");
             // Sending a get request with the given movie to the API 
             console.log("");
 
-            $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=" + film, function (json) {
+            $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=" + film, function(json) {
                 if (json.results[0] != null) {
 
                     // Getting title
@@ -69,41 +76,42 @@ $(document).ready(function () {
                         },
 
                         // Inserting recommendation into text
-                        success: function (response) {
+                        success: function(response) {
                             if (response.Similar.Results[0] != null) {
                                 recommendation = response.Similar.Results[0].Name;
 
                                 $('#recommendation').html('<div id="title"><h2>If you like: ' + searchedTitle + ' then you\'ll probably also enjoy: ' + recommendation + '</h2></div>');
-
+                                console.log(recommendation);
                                 // Getting poster and movie id for recommendation
 
-                                $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=" + recommendation, function () {
+                                $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=" + recommendation, function() {
                                     if (json.results[0] != null) {
-                                        console.log("");
                                         recommendationMovieId = json.results[0].id;
+                                        console.log("reco ID" + recommendationMovieId)
+                                        recommendationPosterPath = json.results[0].poster_path;
+                                        console.log("Reco poster path" + recommendationPosterPath);
                                         if (json.results[0].poster_path != null) {
-                                            $('#poster').html('<h2 class="loading"><img id="thePoster" src=' + poster_base_url + imagesize + json.results[0].poster_path + ' /></h2>');
-                                            $('#thePoster').on('load', function () {
-                                                // hide the loading image
-                                                $("#loadingDiv").hide();
-                                            });
+                                            // $("#thePoster").attr('src','img url');
+                                            $("#thePoster").attr("src", "" + poster_base_url + imagesize + recommendationPosterPath + "");
+                                            // $('.poster').html('<h3 class="loading"><img id="thePoster" class="img-responsive" src=' + poster_base_url + imagesize + json.results[0].poster_path + ' /></h3>');
 
-
-                                        }
-                                        else {
+                                        } else {
                                             $('#recommendation').html('<h2 class="loading">Sorry, no poster found poster for ' + recommendation + '</h2>');
                                         }
-                                        // Getting plot info for recommendation
-                                        $.getJSON("https://api.themoviedb.org/3/movie/" + recommendationMovieId + "?api_key=" + api_key + "", function (json) {
-                                            $('#info').html('<h4 id="plot" class="loading">' + json.overview + '</h4>');
+                                        // Getting info for recommendation
+                                        $.getJSON("https://api.themoviedb.org/3/movie/" + recommendationMovieId + "?api_key=" + api_key + "", function(json) {
+                                            $('#plot').html('<h4 id="resultPlot">' + json.overview + '</h4>');
+                                            $('#releaseYear').html('<h4 id="resultYear">' + json.release_date + '</h4>');
+                                            $('#runtime').html('<h4 id="resultRuntime">' + json.runtime + '</h4>');
+                                            // $('#genre').html('<h4 id="resultGenre">' + json.GENRE + '</h4>');
                                         });
 
                                         // Getting cast for recommendation
-                                        $.getJSON("http://api.themoviedb.org/3/movie/" + recommendationMovieId + "/casts?api_key=" + api_key + "", function (json) {
+                                        $.getJSON("https://api.themoviedb.org/3/movie/" + recommendationMovieId + "/casts?api_key=" + api_key + "", function(json) {
                                             if (json.cast[0] != null) {
-                                                $('#director').html('<h4 id="director" class="loading">' + json.crew[0].name + '</h4>');
+                                                $('#director').html('<h4 id ="resultDirector">' + json.crew[0].name + '</h4>');
                                             } else {
-                                                $('#director').html('<h4 id="director" class="loading">No director found for: ' + recommendation + '</h4>');
+                                                $('#director').html('<h4 id="resultDirector"">No director found for: ' + recommendation + '</h4>');
                                             }
                                         });
                                     } else {
@@ -114,11 +122,10 @@ $(document).ready(function () {
                                 });
 
 
-                            }
-                            else {
+                            } else {
                                 console.log("Couldn't find recommendation");
                                 //Poster for recommendation not found
-                                $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=themask", function (json) {
+                                $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=themask", function(json) {
                                     console.log("Line 95");
                                     // clearAll();
                                     $('#recommendation').html('<h2 class="loading">Sorry, but we couldnt find any recommendation for ' + searchedTitle + '</h2>');
@@ -130,9 +137,12 @@ $(document).ready(function () {
 
                     // Couldn't find recommendation at all    
                 } else {
-                    $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=goonies", function (json) {
+                    $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=goonies", function(json) {
                         console.log("Line 105");
-                        $('#recommendation').html('<h2>Sorry, but we couldnt find ' + film + ' but maybe this poster will do instead?</h2><img id="thePoster" src=' + poster_base_url + imagesize + json.results[0].poster_path + ' />');
+                        $('#recommendation').html('<h2>Sorry, but we couldnt find ' + film + ' but maybe this poster will do instead?</h2>');
+                        $("#thePoster").attr("src", "" + poster_base_url + imagesize + json.results[0].poster_path + "");
+
+
                     });
                 }
             });
@@ -144,7 +154,7 @@ $(document).ready(function () {
 
 
     $('#search').click(getPoster);
-    $('#term').keyup(function (event) {
+    $('#term').keyup(function(event) {
         if (event.keyCode == 13) {
             getPoster();
         }
--- js/script.js
+++ js/script.js
@@ -3,55 +3,67 @@
 $main = $("body");
 
 $(document).on({
-    ajaxStart: function (ajaxSearch) { $main.addClass("loadingSymbol"); },
-    ajaxStop: function (ajaxStop) { $main.removeClass("loadingSymbol"); }
+    ajaxStart: function(ajaxSearch) { $main.addClass("loadingSymbol"); },
+    ajaxStop: function(ajaxStop) { $main.removeClass("loadingSymbol"); }
 });
 
 
 
-$(document).ready(function () {
+$(document).ready(function() {
+    var imagesize = "w342/"
+    var poster_base_url = "https://image.tmdb.org/t/p/";
 
+    var api_key = "f6ab596ab59d550625734987a1ea07fb";
+    var searchedTitle;
+    var searchedMovieID;
+    var recommendation;
+    var recommendationMovieId;
+    var director;
+    var recommendationPosterPath;
+    var ajaxCall;
     // Adjusts the window size when document is loaded (needed for background to work properly in Edge and Firefox)
-    $(function () {
+    $(function() {
         $(".fill-screen").css("height", window.innerHeight);
     });
 
     //  Adjusts the background image after windows size when window size is changed
-    $(window).on("load resize", function () {
+    $(window).on("load resize", function() {
         $(".fill-screen").css("height", window.innerHeight);
     });
 
-    // Method to clear all earlier results
-    var clearAll = function () {
+    // Method to clear all earlier results from .clear-classes
+    var clearAll = function() {
         var full = $(".clean");
         $('.clean').empty();
     }
 
+    // var clearPoster = function() {
+    //     var full = $(".cleanPoster");
+    //     $('.cleanPoster').empty();
+    //     $('.poster').html('<img id="thePoster" class="img-responsive clean" src="img/placeholder.png" />');
+
+    // }
+
     //Main method for getting poster
-    var getPoster = function () {
-        var imagesize = "w342/"
-        var poster_base_url = "https://image.tmdb.org/t/p/";
+    var getPoster = function() {
         var film = $('#term').val();
-        var api_key = "f6ab596ab59d550625734987a1ea07fb";
-        var searchedTitle;
-        var searchedMovieID;
-        var recommendation;
-        var recommendationMovieId;
-        var director;
+
 
         // $( "#info" ).empty();
         clearAll();
 
         if (film == '') {
-            $('#poster').html("<h2 class='loading'>Please type something :-) </h2>");
+            $('#recommendation').html("<h2 class='loading'>Please type something :-) </h2>");
+            $("#thePoster").attr("src", "img/placeholder.png");
         } else {
             $('#recommendation').html("<h2 class='loading'>We're Looking for your poster!</h2>");
             // Sending a get request with the given movie to the API 
             console.log("");
 
-            $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=" + film, function (json) {
+            $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=" + film, function(json) {
+                console.log(json)
                 if (json.results[0] != null) {
-
+                    ajaxCall = "https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=" + film;
                     // Getting title
                     searchedTitle = json.results[0].title;
                     searchedMovieID = json.results[0].id;
@@ -67,69 +79,38 @@ $(document).ready(function () {
                         },
 
                         // Inserting recommendation into text
-                        success: function (response) {
-                            if (response.Similar.Results[0] != null) {
-                                recommendation = response.Similar.Results[0].Name;
+                        success: function(response) {
+                                if (response.Similar.Results[0] != null) {
+                                    recommendation = response.Similar.Results[0].Name;
 
-                                $('#recommendation').html('<div id="title"><h2>If you like: ' + searchedTitle + ' then you\'ll probably also enjoy: ' + recommendation + '</h2></div>');
-
-                                // Getting poster and movie id for recommendation
-
-                                $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=" + recommendation, function () {
-                                    if (json.results[0] != null) {
-                                        console.log("");
-                                        recommendationMovieId = json.results[0].id;
-                                        if (json.results[0].poster_path != null) {
-                                            $('#poster').html('<h2 class="loading"><img id="thePoster" src=' + poster_base_url + imagesize + json.results[0].poster_path + ' /></h2>');
-                                            $('#thePoster').on('load', function () {
-                                             
-                                            });
+                                    $('#recommendation').html('<div id="title"><h2>If you like: ' + searchedTitle + ' then you\'ll probably also enjoy: ' + recommendation + '</h2></div>');
+                                    console.log(ajaxCall);
+                                    makeAJAXcall();
 
 
-                                        }
-                                        else {
-                                            $('#recommendation').html('<h2 class="loading">Sorry, no poster found poster for ' + recommendation + '</h2>');
-                                        }
-                                        // Getting plot info for recommendation
-                                        $.getJSON("https://api.themoviedb.org/3/movie/" + recommendationMovieId + "?api_key=" + api_key + "", function (json) {
-                                            $('#info').html('<h4 id="plot" class="loading">' + json.overview + '</h4>');
-                                        });
 
-                                        // Getting cast for recommendation
-                                        $.getJSON("https://api.themoviedb.org/3/movie/" + recommendationMovieId + "/casts?api_key=" + api_key + "", function (json) {
-                                            if (json.cast[0] != null) {
-                                                $('#director').html('<h4 id="director" class="loading">' + json.crew[0].name + '</h4>');
-                                            } else {
-                                                $('#director').html('<h4 id="director" class="loading">No director found for: ' + recommendation + '</h4>');
-                                            }
-                                        });
-                                    } else {
-                                        //Poster for recommendation not found
-                                        console.log("Line 85");
-                                        $('#recommendation').html('<h2 class="loading">Sorry, but we couldn\'t find' + recommendation + ' </h2>');
-                                    }
-                                });
-
-
-                            }
-                            else {
-                                console.log("Couldn't find recommendation");
-                                //Poster for recommendation not found
-                                $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=themask", function (json) {
-                                    console.log("Line 95");
-                                    // clearAll();
-                                    $('#recommendation').html('<h2 class="loading">Sorry, but we couldnt find any recommendation for ' + searchedTitle + '</h2>');
-                                });
-                            }
-                        }
+                                } else {
+                                    console.log("Couldn't find recommendation");
+                                    //Poster for recommendation not found
+                                    $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=themask", function(json) {
+                                        console.log("Line 95");
+                                        // clearAll();
+                                        $('#recommendation').html('<h2 class="loading">Sorry, but we couldnt find any recommendation for ' + searchedTitle + '</h2>');
+                                        $("#thePoster").attr("src", "img/placeholder.png");
+                                    });
+                                }
+                            } // Stop
                     });
 
 
                     // Couldn't find recommendation at all    
                 } else {
-                    $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=goonies", function (json) {
+                    $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=goonies", function(json) {
                         console.log("Line 105");
-                        $('#recommendation').html('<h2>Sorry, but we couldnt find ' + film + ' but maybe this poster will do instead?</h2><img id="thePoster" src=' + poster_base_url + imagesize + json.results[0].poster_path + ' />');
+                        $('#recommendation').html('<h2>Sorry, but we couldnt find ' + film + ' but maybe this poster will do instead?</h2>');
+                        $("#thePoster").attr("src", "" + poster_base_url + imagesize + json.results[0].poster_path + "");
+
+
                     });
                 }
             });
@@ -141,13 +122,59 @@ $(document).ready(function () {
 
 
     $('#search').click(getPoster);
-    $('#term').keyup(function (event) {
+    $('#term').keyup(function(event) {
         if (event.keyCode == 13) {
             getPoster();
         }
     });
+    // ajaxCall2 = $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=" + recommendation);
+    // Getting poster and movie id for recommendation
+    var makeAJAXcall = function() {
+        $.when($.getJSON(ajaxCall), $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=" + recommendation)).done(function(result1, result2) {
 
+            if (result2 != null) {
+                console.log(result2[0].results[0].id);
+                // console.log(result2[0].object.results[0].object.id);
+                recommendationMovieId = result2[0].results[0].id;
+                console.log("reco ID" + recommendationMovieId);
+                recommendationPosterPath = result2[0].results[0].poster_path;
+                // console.log("Reco poster path" + recommendationPosterPath);
+                // console.log(json.results[0].original_title)
+                if (result2[0].results[0].poster_path != null) {
+                    // $("#thePoster").attr('src','img url');
+                    $("#thePoster").attr("src", "" + poster_base_url + imagesize + recommendationPosterPath + "");
+                    // $('.poster').html('<h3 class="loading"><img id="thePoster" class="img-responsive" src=' + poster_base_url + imagesize + json.results[0].poster_path + ' /></h3>');
 
+                } else {
+                    $('#recommendation').html('<h2 class="loading">Sorry, no poster found poster for ' + recommendation + '</h2>');
+                }
+                // Getting info for recommendation
+                $.getJSON("https://api.themoviedb.org/3/movie/" + recommendationMovieId + "?api_key=" + api_key + "", function(json) {
+                    $('#plot').html('<h4 id="resultPlot">' + json.overview + '</h4>');
+                    $('#releaseYear').html('<h4 id="resultYear">' + json.release_date + '</h4>');
+                    $('#runtime').html('<h4 id="resultRuntime">' + json.runtime + '</h4>');
+                    // $('#genre').html('<h4 id="resultGenre">' + json.GENRE + '</h4>');
+                });
 
+                // Getting cast for recommendation
+                $.getJSON("https://api.themoviedb.org/3/movie/" + recommendationMovieId + "/casts?api_key=" + api_key + "", function(json) {
+                    if (json.cast[0] != null) {
+                        $('#director').html('<h4 id ="resultDirector">' + json.crew[0].name + '</h4>');
+                    } else {
+                        $('#director').html('<h4 id="resultDirector"">No director found for: ' + recommendation + '</h4>');
+                    }
+                });
+            } else {
+                //Poster for recommendation not found
+                console.log("Line 85");
+                $('#recommendation').html('<h2 class="loading">Sorry, but we couldn\'t find' + recommendation + ' </h2>');
+            }
+        });
+    }
 });
+
+
+
+
+//  });
 // Coded by Alexander Vikenfalk
